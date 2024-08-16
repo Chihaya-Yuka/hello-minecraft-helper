b04533d1afa96f31b9dd18ef2e7c415401f27114
Chihaya-Yuka <yuka-chihaya@outlook.com>
2024-08-16 15:19:15 +0800

    feat: 添加配置文件

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import sys
+
+def system_info() -> int:
+    platform = sys.platform
+    if platform == 'win32':
+        return 1
+    else:
+        return 0
+
+class config:
+    VERSION = '3.0.1'
+    COMPANY = 'Huanyu, Inc.'
+    RUNTIME = 'Kivy 2.3.0'
+    SYSTEM = system_info()
