54cadca8ca035ab8138d976d93ab1bb321f1edd4
Yuka <admin@awaland.xyz>
2024-08-20 16:27:59 +0800

    feat: 使用面向对象设计方法重构

---
+++ src/多玩我的世界盒子.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import subprocess
+
+class Setup:
+    def __init__(self):
+        self.is_first_open = os.path.exists('First Open')
+        self.commands = [
+            'python -m pip install --upgrade pip wheel setuptools',
+            'python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew',
+            'python -m pip install kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/',
+            'python -m pip install kivy',
+        ]
+
+    def run_commands(self):
+        for command in self.commands:
+            subprocess.run(command, shell=True, check=True)
+
+    def update_path(self):
+        if sys.platform == 'win32' and self.is_first_open:
+            new_path = f"{os.environ['PATH']};{os.getcwd()}\\share\\sdl2\\bin;{os.getcwd()}\\share\\glew\\bin"
+            os.environ['PATH'] = new_path
+
+    def setup_environment(self):
+        self.run_commands()
+        self.update_path()
+
+if __name__ == "__main__":
+    setup = Setup()
+    setup.setup_environment()
+    subprocess.run(['python', 'main.py'])
