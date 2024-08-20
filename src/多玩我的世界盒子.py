aaf1365dc1273d9c5692deeb85aca6e656fa24e8
Yuka <admin@awaland.xyz>
2024-08-20 16:28:51 +0800

    debug: 修复列表中的语法错误

--- src/多玩我的世界盒子.py
+++ src/多玩我的世界盒子.py
@@ -9,7 +9,7 @@ class Setup:
             'python -m pip install --upgrade pip wheel setuptools',
             'python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew',
             'python -m pip install kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/',
-            'python -m pip install kivy',
+            'python -m pip install kivy'
         ]
 
     def run_commands(self):
